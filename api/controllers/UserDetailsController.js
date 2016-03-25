864458253efbb8b13aa0ea0091f0e924735a8419
Din <din.tamari@usi.ch>
2016-03-25 11:34:24 +0100

    Added userDetails model + controller

---
+++ api/controllers/UserDetailsController.js
@@ -0,0 +1,11 @@
+/**
+ * UserDetailsController
+ *
+ * @description :: Server-side logic for managing userdetails
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
